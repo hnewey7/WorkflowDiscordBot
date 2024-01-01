e6a8779bcf8e56674fd51f5ec5d07d57bdeb4248
hnewey7 <hnewey7@gmail.com>
2024-01-01 21:00:12 +0000

    Initial working bot

---
+++ bot.py
@@ -0,0 +1,12 @@
+import discord
+
+def run_discord_bot():
+    TOKEN = 'MTE5MTQ0NzQ5OTM4NzQzNzEwOA.GgSomy.bK3obSpCL8KdShCpJss8zyw3DOFcb5saIL785g'
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running.')
+
+    client.run(TOKEN)
