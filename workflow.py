77fe42917d30c5a7d733059cb52a8973c5eeacdd
hnewey7 <hnewey7@gmail.com>
2024-01-02 15:49:17 +0000

    Basic Workflow object for tracking projects

---
+++ workflow.py
@@ -0,0 +1,28 @@
+'''
+Workflow Object
+@author: Harry New
+2nd Jan 2024
+'''
+
+class Workflow():
+
+    def __init__(self) -> None:
+        # Storing projects in dictionary.
+        self.projects = []
+
+    # Create new project from title and add to dictionary.
+    def add_project(self, title, deadline) -> None:
+        new_project = Project(title, deadline)
+        self.projects.append(new_project)
+
+    # Remove existing project.
+    def del_project(self,project) -> None:
+        self.projects.remove(project)
+
+
+class Project():
+
+    def __init__(self,deadline) -> None:
+        self.tasks = []
+        self.deadline = deadline
+
