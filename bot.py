11d5a4c59c7c0cf9bb84e8ad02ea5e5117f9faa9
hnewey7 <hnewey7@gmail.com>
2024-01-05 22:18:04 +0000

    Created !projects bot using UI features, no backend yet

--- bot.py
+++ bot.py
@@ -17,10 +17,25 @@ def run_discord_bot():
     print("New workflow created.")
 
     @bot.command()
-    async def button(ctx):
+    async def projects(ctx):
+        # Creating content of message.
+        content = '>>> # Existing Projects:\n'
+
+        if len(workflow.projects) != 0:
+            for project in workflow.projects:
+                content += f'{workflow.projects.index(project)+1}. {project.title} - Deadline <t:{project.get_unit_deadline()}:R>\n' if project.deadline else \
+                f'{workflow.projects.index(project)+1}. {project.title}\n'
+        else:
+            content += '### No existing projects.\n '
+
+        # Creating UI at bottom of message.
         view = discord.ui.View()
-        button = discord.ui.Select(min_values=1,max_values=1,options=[discord.SelectOption(label="test")],disabled=False)
-        view.add_item(button)
-        await ctx.send(view=view)
+        add_project_button = discord.ui.Button(label="Add Project")
+        del_project_button = discord.ui.Button(label="Delete Project")
+        view.add_item(add_project_button)
+        view.add_item(del_project_button)
+
+        await ctx.send(content=content,view=view,delete_after=300)
+
 
     bot.run(TOKEN)
