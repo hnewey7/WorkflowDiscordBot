5fbc16b2dd844004842289a1b9624e51cbf37e35
hnewey7 <hnewey7@gmail.com>
2024-01-06 21:13:41 +0000

    Modified output to use embeds

--- bot.py
+++ bot.py
@@ -120,25 +120,28 @@ def run_discord_bot():
 
         while True:
             # Creating content of message.
-            content = '>>> # Existing Projects:\n'
+            content = ''
 
             if len(workflow.projects) != 0:
                 for project in workflow.projects:
-                    content += f'### {workflow.projects.index(project)+1}. {project.title} - Deadline <t:{project.get_unix_deadline()}:R>\n' if project.deadline else \
+                    content += f'{workflow.projects.index(project)+1}. {project.title} - Deadline <t:{project.get_unix_deadline()}:R>\n' if project.deadline else \
                     f'{workflow.projects.index(project)+1}. {project.title}\n'
             else:
-                content += '### No existing projects.\n '
+                content += 'No existing projects.\n '
+
+            # Creating embed for message.
+            embed = discord.Embed(color=discord.Color.yellow(),title="Existing Projects",description=content)
 
             # Creating UI at bottom of message.
             view = ProjectButtonView(workflow=workflow)
 
             # Updating message.
             if initial_check:
-                message = await ctx.send(content=content,view=view,delete_after=300)
+                message = await ctx.send(embed=embed,view=view,delete_after=300)
                 initial_check = False
                 await bot.wait_for('message_delete')
             else:
-                await message.edit(content=content,view=view,delete_after=300)
+                await message.edit(embed=embed,view=view,delete_after=300)
                 await bot.wait_for('message_delete')
 
     bot.run(TOKEN)
