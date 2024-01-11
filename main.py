d5a2dd5cd9065d49b667c684b76a57439b58ded9
hnewey7 <hnewey7@gmail.com>
2024-01-11 23:07:01 +0000

    Converted to client version, with additional features for joining new servers

--- main.py
+++ main.py
@@ -6,7 +6,41 @@ Created on Monday 1st January 2024
 
 '''
 
-import bot
+import discord
+
+# - - - - - - - - - - - - - - - - - - - - - - - 
+
+def init_client(token):
+    # Initialising and running client.
+    client = discord.Client(intents=intents)
+    # Initialising events.
+    init_events(client)
+    print("Initialising events.")
+    print("Running client.")
+    client.run(TOKEN,reconnect=True)
+    return client
+
+def init_events(client):
+    # On guild join event.
+    @client.event
+    async def on_guild_join(guild):
+        print(f"Joined discord server ({guild.name}).")
+        # Creating "Workflow" category.
+        await guild.create_category(name="Workflow",)
+        print('Created category ("Workflow")')
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - 
 
 if __name__ == "__main__":
-    bot.run_discord_bot()
+    # Initialising intents.
+    TOKEN = 'MTE5MTQ0NzQ5OTM4NzQzNzEwOA.GgSomy.bK3obSpCL8KdShCpJss8zyw3DOFcb5saIL785g'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.guilds = True
+    # Initialising client.
+    client = init_client(TOKEN)
+
+
+
+    
