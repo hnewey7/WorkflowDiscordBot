81f7fcff57f2206bbbb75c47e49da4e3a4816c48
hnewey7 <hnewey7@gmail.com>
2024-02-11 19:17:14 +0000

    Deleting command messages from users.

--- commands/__init__.py
+++ commands/__init__.py
@@ -27,28 +27,37 @@ async def evaluate_command(command, client, workflow):
     if "help" == command.content[1:]:
         logger.info("Requesting help command.")
         await help_command(command,client,workflow)
+        await command.delete()
     if "set_active_channel" == command.content[1:]:
         logger.info("Requesting set active channel.")
         await set_active_channel_command(command, workflow, client)
+        await command.delete()
     if "manage_projects" == command.content[1:]:
         logger.info("Requesting manage projects.")
         await manage_projects(command,client,workflow)
+        await command.delete()
     if "manage_tasks" == command.content[1:]:
         logger.info("Requesting manage tasks.")
         await manage_tasks(command,client,workflow)
+        await command.delete()
     if "teams" == command.content[1:]:
         logger.info("Requesting teams.")
         await display_teams(command,workflow,client)
+        await command.delete()
     if "disconnect" == command.content[1:]:
         logger.info("Requesting disconnect.")
         await disconnect_command(client)
+        await command.delete()
     if "show_workflow" == command.content[1:]:
         logger.info("Requesting show workflow.")
         await show_workflow_command(workflow)
+        await command.delete()
     if "show_guild" == command.content[1:]:
         logger.info("Requesting show guild.")
         await show_guild_command(command)
+        await command.delete()
     if "delete_roles" == command.content[1:]:
         logger.info("Requesting deleting all roles.")
         await delete_roles_command(command)
+        await command.delete()
 
