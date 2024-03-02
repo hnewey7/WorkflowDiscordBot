0c25c6cc43c455eff7bdbc08f9631cf5404f6425
hnewey7 <hnewey7@gmail.com>
2024-03-02 11:42:58 +0000

    Displaying exceptions on reset_server command.

--- main.py
+++ main.py
@@ -293,10 +293,15 @@ def init_commands(client,tree):
   @is_developer()
   async def reset_server(interaction):
     logger.info("Requesting reset server command.")
-    del workflows[str(interaction.guild.id)]
-
-    workflows[str(interaction.guild.id)] = Workflow()
-    await interaction.response.send_message("WorkflowBot has been reset for this server.",ephemeral=True)
+    try:
+      # Deleting original Workflow.
+      del workflows[str(interaction.guild.id)]
+      # Creating new Workflow.
+      workflows[str(interaction.guild.id)] = Workflow()
+      # Sending message.
+      await interaction.response.send_message("WorkflowBot has been reset for this server.",ephemeral=True)
+    except Exception as e:
+      logger.error(f"Error: {e}")
 
 
 async def init_saved(client):
